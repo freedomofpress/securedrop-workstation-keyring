1204566ac834b4b4113ed6dbb19d528da1363686
deeplow <francisco@freedom.press>
2026-01-15 13:27:23 +0000

    Add support for local development (BRANCH=local)
    Supports building from local branch by skipping the fetch stage in the
    Qubes Builder. This improves the local development process.

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -15,21 +15,36 @@ set -o pipefail
 
 echo "Copy sd-builder.yml into qubes-builderv2 repo"
 cp sd-qubes-builder/sd-builder.yml.conf ../qubes-builderv2/sd-builder.yml
-
-echo "Build from ${BRANCH}"
 sed -i "s/{{branch}}/${BRANCH}/g" "../qubes-builderv2/sd-builder.yml"
 
 echo "Remove old build artifacts if present"
 rm -rf build || true
 
 # qubes-builderv2 repo clones and caches a copy of the target
-# git repository. If building from branches that have had
-# force-pushes, or if switching branches, stale git artifacts
-# or a requirement for a rebase can break the qubes-builder
-# automation. 
 echo "Remove SDW-keyring sources from qubes-builder"
 rm -rf ../qubes-builderv2/artifacts/sources/securedrop-workstation-keyring || true
 rm -rf ../qubes-builderv2/artifacts/repository/*/securedrop-workstation-keyring* || true
+
+if [ "${BRANCH}" == "local" ]; then
+    # OPTION 1: BUILDING FROM LOCAL DIRECTORY
+    # Replace Qubes Builder's fetch with a symlink to this repo's directory
+    REPO_ROOT=$(dirname $(dirname $(realpath $0)))
+    if [ ! -d "$REPO_ROOT/.git" ]; then  # Check in case script changes location
+        echo "ERROR: $REPO_ROOT not project root" >&2 && exit 1
+    fi
+
+    # Create sources directory (needed in first run)
+    mkdir -p ../qubes-builderv2/artifacts/sources/
+
+    ln -s $REPO_ROOT ../qubes-builderv2/artifacts/sources/securedrop-workstation-keyring
+    SKIP_GIT_FETCH="true"
+else
+    # OPTION 2: BUILDING FROM BRANCH
+    # If building from branches that have had force-pushes, or if
+    # switching branches, stale git artifacts or a requirement for a rebase can
+    # break the qubes-builder automation. Therefore, these sources are re-fetched.
+    echo "Build from ${BRANCH}"
+fi
 
 echo "Begin build for Qubes Version ${QUBES_RELEASE} (${FEDORA_DIST})"
 (
@@ -39,6 +54,7 @@ echo "Begin build for Qubes Version ${QUBES_RELEASE} (${FEDORA_DIST})"
       --option qubes-release=r${QUBES_RELEASE} \
       --option executor:type="${EXECUTOR}" \
       --option executor:options:"${EXECUTOROPTS}" \
+      --option skip-git-fetch=${SKIP_GIT_FETCH:-'false'} \
       --option +distributions+"${FEDORA_DIST}" \
       -d "${FEDORA_DIST}" \
       -c securedrop-workstation-keyring package fetch prep build
